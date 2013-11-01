1a0ba73ede85dfa7ab3fb5ec8ad09c0edb63217d
Peter Keating <peter@moov2.com>
2013-11-01 16:11:44 +0000

    players are randomized to determine the start order.

--- assets/js/domain/match.js
+++ assets/js/domain/match.js
@@ -11,9 +11,31 @@ function () {
 
     var p = Match.prototype = {};
 
+    /**
+     * Randomly sorts the players.
+     * Source: http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/12646864#12646864
+     */
+    p.shufflePlayers = function () {
+        for (var i = this.players.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = this.players[i];
+            this.players[i] = this.players[j];
+            this.players[j] = temp;
+        }
+    };
+
+    /**
+     * Sets the order of the current players, this depicts the starting order.
+     */
+    p.setOrder = function () {
+        // later on the order will be determine by previous matches.
+        this.randomizePlayers();
+    };
+
     p.setPlayers = function (players) {
     	this.players = players;
-    	//setup match starting time, pity points etc
+
+        this.setOrder();
     };
 
     return new Match();
