0d11ff0fe1217a90df6741574f8190f9f5e41334
Lawrence Dine <lawrence.dine@moov2.com>
2013-11-01 15:05:21 +0000

    Update state component to display state template when attached.

--- assets/js/ui/state.js
+++ assets/js/ui/state.js
@@ -1,10 +1,20 @@
 define([
-	'flight'
+	'flight',
+	'handlebars',
+	'text!templates/state.hbs'
 ],
-function (Flight) {
+function (Flight, Handlebars, stateHtml) {
 	
 	function State() {
-		
+		this.template = Handlebars.compile(stateHtml);
+
+		this.after('initialize', function () {
+			this.render();
+		});
+
+		this.render = function () {
+			this.$node.html(this.template());
+		};
 	}
 
 	return flight.component(State);
