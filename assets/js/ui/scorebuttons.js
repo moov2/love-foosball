4d70281986a4f731f2fed702b23f778f77c0a974
Lawrence Dine <lawrence.dine@moov2.com>
2013-11-01 17:07:54 +0000

    Update scorebuttons component to use JS to centre the score buttons

--- assets/js/ui/scorebuttons.js
+++ assets/js/ui/scorebuttons.js
@@ -8,11 +8,24 @@ function (Flight, Handlebars, scorebuttonHtml) {
 	function ScoreButtons () {
 		this.template = Handlebars.compile(scorebuttonHtml);
 
+		this.defaultAttrs({
+            scoreButtonList: '.js-scorebuttons-list',
+            scoreButtonItems: '.js-scorebuttons-list > li'
+        });
+
 		this.after('initialize', function () {
 			this.leftPlayer = this.attr.game.players[0];
 			this.rightPlayer = this.attr.game.players[1];
 
 			this.render();
+
+			var contentWidth = 0;
+
+            this.select('scoreButtonItems').each(function() {
+                contentWidth += $(this).outerWidth( true );
+            });
+
+            this.select('scoreButtonList').css('width', (contentWidth + 20) + 'px');
 		});
 
 		this.render = function () {
