e1d0eb2c24c9583e925c3889d3d9460ea7d0ad6c
Peter Keating <peter@moov2.com>
2013-11-02 12:28:34 +0000

    removed rendering glitch that occurs when a player has scored.

--- assets/js/ui/scoreboard.js
+++ assets/js/ui/scoreboard.js
@@ -9,6 +9,8 @@ function (Flight, Handlebars, scoreboardHtml) {
 	function Scoreboard() {
 		this.template = Handlebars.compile(scoreboardHtml);
 
+		this.hasRendered = false;
+
 		this.defaultAttrs({
             playerQueue: '.js-player-queue',
             firstPlayer: '.js-first-player',
@@ -28,9 +30,15 @@ function (Flight, Handlebars, scoreboardHtml) {
 		 * Handle the first player scoring a goal.
 		 */
 		this.firstPlayerScored = function () {
-			console.log('first player scored');
-			this.attr.game.scored(this.attr.game.players[0]);
-			this.render();
+			this.attr.game.scored(this.getFirstPlayer());
+			this.updatePlayersInView();
+		};
+
+		/**
+		 * Returns the first player.
+		 */
+		this.getFirstPlayer = function () {
+			return this.attr.game.players[0];
 		};
 
 		/**
@@ -47,21 +55,50 @@ function (Flight, Handlebars, scoreboardHtml) {
 			return waitingPlayers;
 		};
 
+		/**
+		 * Returns the second player.
+		 */
+		this.getSecondPlayer = function () {
+			return this.attr.game.players[1];
+		};
+
 		this.render = function () {
 			this.$node.html(this.template({
-				firstPlayer: this.attr.game.players[0],
-				secondPlayer: this.attr.game.players[1],
+				firstPlayer: this.getFirstPlayer(),
+				secondPlayer: this.getSecondPlayer(),
 				waitingPlayers: this.getPlayerQueue()
 			}));
+
+			this.hasRendered = true;
 		};
 
 		/**
 		 * Handles the second player scoring a goal.
 		 */
 		this.secondPlayerScored = function () {
-			console.log('second player scored');
-			this.attr.game.scored(this.attr.game.players[1]);
-			this.render();
+			this.attr.game.scored(this.getSecondPlayer());
+			this.updatePlayersInView();
+		};
+
+		/**
+		 * To avoid re-rendering the whole view, lets just update the required
+		 * attributes.
+		 */
+		this.updatePlayersInView = function () {
+			// first player.
+			this.select('firstPlayer').find('.js-player-thumbnail').attr('src', this.getFirstPlayer().photo);
+			this.select('firstPlayer').find('.js-player-score').html(this.getFirstPlayer().score);
+
+			// second player
+			this.select('secondPlayer').find('.js-player-thumbnail').attr('src', this.getSecondPlayer().photo);
+			this.select('secondPlayer').find('.js-player-score').html(this.getSecondPlayer().score);
+
+			// update the player queue.
+			var waitingPlayers = this.getPlayerQueue();
+
+			for (var i = 0; i < waitingPlayers.length; i++) {
+				this.select('playerQueue').find('li:eq(' + i + ') > .js-player-thumbnail').attr('src', waitingPlayers[i].photo);
+			}
 		};
 	}
 
